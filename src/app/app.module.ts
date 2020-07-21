d39e4791b38d8edf3be4fe0ec562fa5786c1e3a6
Eddie <eddie@cpe-107-11-52-19.columbus.res.rr.com>
2020-07-20 22:03:04 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './auth/login/login.component';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+import { ToastrModule } from 'ngx-toastr';
+import { HomeComponent } from './component/home/home.component';
+import { PostComponent } from './component/post/post.component';
+import { SubredditComponent } from './component/subreddit/subreddit.component';
+import { PostTileComponent } from './component/shared/post-tile/post-tile.component';
+import { SideBarComponent } from './component/shared/side-bar/side-bar.component';
+import { SubredditSideBarComponent } from './component/shared/subreddit-side-bar/subreddit-side-bar.component';
+import { VoteButtonComponent } from './component/shared/vote-button/vote-button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupComponent,
+    LoginComponent,
+    UserProfileComponent,
+    HomeComponent,
+    PostComponent,
+    SubredditComponent,
+    PostTileComponent,
+    SideBarComponent,
+    SubredditSideBarComponent,
+    VoteButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxWebstorageModule.forRoot(),
+    ToastrModule.forRoot(),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
